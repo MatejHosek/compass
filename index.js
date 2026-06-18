ed0195e5c8b2e870028b4b5a597f89e978de84e6
Matěj Hošek <matthy.hosek@gmail.com>
2026-06-18 16:26:47 +0200

    Setup map, geolocation and user marker

---
+++ index.js
@@ -0,0 +1,44 @@
+var position = [0, 0];
+
+function geolocate() {
+    var userMarker = new Object();
+
+    navigator.geolocation.getCurrentPosition(g => {
+        position = [g.coords.latitude, g.coords.longitude];
+        map.panTo(position)
+
+        userMarker['marker'] = L.marker(position, {
+            icon: L.icon({
+                iconUrl: 'leaflet/images/marker-location.png',
+
+                iconSize:     [30, 30],
+                iconAnchor:   [10, 10],
+
+                rotationOrigin: 'center',
+                // TODO: Marker rotation
+            }),
+            keyboard: false,
+        }).addTo(map);
+    });
+
+    navigator.geolocation.watchPosition(g => {
+        position = [g.coords.latitude, g.coords.longitude];
+        
+        // TODO: Marker rotation
+        // TODO: Recalculate distance to Prague
+    })
+}
+
+function initMap() {
+    map = L.map('map').setView(position, 15);
+
+    L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    return map;
+}
+
+var map = initMap();
+geolocate();
